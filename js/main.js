d28866d591f94b9df24c5fc220b8109a2c6e2d05
cindy <cindialy@gmail.com>
2017-07-03 16:55:30 -0400

    avance

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+$(document).ready(function(){
+
+    $('#registro').click(function(){
+	   var nombre= $('#name').val();
+	   var apellido= $('#lastname').val();
+	   var correo= $('#input-email').val();
+	   var contraseña= $('input-password').val();
+
+	   if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(nombre))){
+           $('.name-container').append('<span>si funciona</span>');
+           $('#name').val("");
+	   }
+    });
+});
