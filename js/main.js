f508bec849a4423010d164d4c5803fa5dd15998d
cindy <cindialy@gmail.com>
2017-07-03 16:58:06 -0400

    avance index

--- js/main.js
+++ js/main.js
@@ -10,5 +10,13 @@ $(document).ready(function(){
            $('.name-container').append('<span>si funciona</span>');
            $('#name').val("");
 	   }
+	   if(!(/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]{2,11})+$/.test(apellido))){
+	   	 $('.lastname-container').append('<span>si funciona</span>');
+           $('#lastname').val("");
+	   }
+	   if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test(correo))){
+         $('.email-container').append('<span>si funciona</span>');
+           $('#input-email').val("");
+	    }
     });
 });
